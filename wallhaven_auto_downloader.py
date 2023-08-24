d7c1a63d9aa852da8a6764fd60c855b04c398b7a
ale3d62 <ale3d62@gmail.com>
2023-08-24 18:43:10 +0200

    first

---
+++ wallhaven_auto_downloader.py
@@ -0,0 +1,41 @@
+from requests import get
+from os import path
+
+username = "ale3d62"
+collectionId = "1591884"
+outDir = "G:\Mi unidad\Mis Archivos\Personal\Wallpapers"
+
+#Downloads the wallpapers from the selected wallhaven collection
+#**The collection has to be public for the script to work**
+
+
+apiResponse = get("https://wallhaven.cc/api/v1/collections/" + username + "/" + collectionId)
+
+if("error" in apiResponse.json()):
+    print("Error during the api request, check the username and the collection Id")
+    exit()
+
+collectionWalls = apiResponse.json()['data']
+currentWallIndex = 1
+collectionSize = len(collectionWalls)
+print("")
+
+for wall in collectionWalls:
+    
+    print("\033[FDownloading " + str(currentWallIndex) + "/" + str(collectionSize))
+    currentWallIndex += 1
+    
+    #Obtain the wall name and extension
+    wallId = wall["id"]
+    wallFileExtension = wallUrl.split('.')[-1]
+    
+    #Check if wall is already downloaded
+    if(not path.isfile(outDir + "\\wallhaven-" + wallId + "." + wallFileExtension)):
+        #Download the wall
+        wallUrl = wall["path"]
+        wallContent = get(wallUrl).content
+        with open(outDir + "\\wallhaven-" + wallId + "." + wallFileExtension, 'wb') as f:
+            f.write(wallContent)
+            f.close()
+
+print("\033[FWallpapers successfully downloaded!")
