b2e3008355d8d3ff540b5907102b73eddd760161
ale3d62 <ale3d62@gmail.com>
2023-09-24 16:01:22 +0200

    bug fixed: the script only downloaded the first page (24 wallpapers) of the collection

--- wallhaven_auto_downloader.py
+++ wallhaven_auto_downloader.py
@@ -1,5 +1,7 @@
+import collections
 from requests import get
 from os import path
+from math import ceil
 
 username = "ale3d62"
 collectionId = "1591884"
@@ -8,34 +10,59 @@ outDir = "G:\Mi unidad\Mis Archivos\Personal\Wallpapers"
 #Downloads the wallpapers from the selected wallhaven collection
 #**The collection has to be public for the script to work**
 
+def downloadWallpapers():
+    #get collection size
+    apiResponse = get("https://wallhaven.cc/api/v1/collections/" + username)
+    if("error" in apiResponse.json()):
+        print("Error during the api request, check the username and the collection Id")
+        return
 
-apiResponse = get("https://wallhaven.cc/api/v1/collections/" + username + "/" + collectionId)
+    collectionSize = 0
 
-if("error" in apiResponse.json()):
-    print("Error during the api request, check the username and the collection Id")
-    exit()
+    userCollections = apiResponse.json()['data']
+    for collection in userCollections:
+        if str(collection['id']) == collectionId:
+            collectionSize = collection['count']
 
-collectionWalls = apiResponse.json()['data']
-currentWallIndex = 1
-collectionSize = len(collectionWalls)
-print("")
+    #there is a maximum of 24 wallpapers per page
+    nCollectionPages = ceil(collectionSize % 24)
 
-for wall in collectionWalls:
-    
-    print("\033[FDownloading " + str(currentWallIndex) + "/" + str(collectionSize))
-    currentWallIndex += 1
-    
-    #Obtain the wall name and extension
-    wallId = wall["id"]
-    wallUrl = wall["path"]
-    wallFileExtension = wallUrl.split('.')[-1]
-    
-    #Check if wall is already downloaded
-    if(not path.isfile(outDir + "\\wallhaven-" + wallId + "." + wallFileExtension)):
-        #Download the wall
-        wallContent = get(wallUrl).content
-        with open(outDir + "\\wallhaven-" + wallId + "." + wallFileExtension, 'wb') as f:
-            f.write(wallContent)
-            f.close()
+    print("")
 
-print("\033[FWallpapers successfully downloaded!")
+    #download wallpapers
+    for currentPage in range(nCollectionPages):
+        apiResponse = get("https://wallhaven.cc/api/v1/collections/" + username + "/" + collectionId + "?page=" + str(currentPage+1))
+
+        if("error" in apiResponse.json()):
+            print("Error during the api request, check the username and the collection Id")
+            return
+
+        collectionWalls = apiResponse.json()['data']
+        currentWallIndex = 1
+        
+
+        for wall in collectionWalls:
+            
+            print("\033[FDownloading " + str(currentWallIndex + currentPage*24) + "/" + str(collectionSize))
+            currentWallIndex += 1
+            
+            #Obtain the wall name and extension
+            wallId = wall["id"]
+            wallUrl = wall["path"]
+            wallFileExtension = wallUrl.split('.')[-1]
+            
+            #Check if wall is already downloaded
+            if(not path.isfile(outDir + "\\wallhaven-" + wallId + "." + wallFileExtension)):
+                #Download the wall
+                wallContent = get(wallUrl).content
+                with open(outDir + "\\wallhaven-" + wallId + "." + wallFileExtension, 'wb') as f:
+                    f.write(wallContent)
+                    f.close()
+
+    print("\033[FWallpapers successfully downloaded!")
+
+
+
+
+
+downloadWallpapers()
